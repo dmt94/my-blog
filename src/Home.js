fd0cda612b2e223ff5b72916ad6e9cdece05b869
Daevah Taylaran <daevtayl@gmail.com>
2023-03-26 15:24:37 -0700

    render child components for home

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -6,14 +7,14 @@ const Home = () => {
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
   ])
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter()
+  }
+
   return ( 
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by: { blog.author }</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs" />
     </div>
    );
 }
