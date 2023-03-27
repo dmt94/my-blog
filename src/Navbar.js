194640b14d75c05447238fc621b60ea8576e32cd
Daevah Taylaran <daevtayl@gmail.com>
2023-03-26 20:54:32 -0700

    change font colors

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return ( 
     <nav className="navbar">
-      <h1>Daevah's Blog</h1>
+      <h1>Tech Blog</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create">New Blog</Link>
